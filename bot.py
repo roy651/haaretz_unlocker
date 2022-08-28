486fb0808a78611606479945a037e2c8f830ab70
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2022-08-28 09:23:58 +0300

    handle text appended at the begining of the link

--- bot.py
+++ bot.py
@@ -4,16 +4,16 @@ from telegram.ext import CommandHandler
 import os
 import re
 
-telegram_bot_token = os.environ['TGRM_TKN']
-updater = Updater(token=telegram_bot_token, use_context=True)
-dispatcher = updater.dispatcher
-
 # set up the introductory statement for the bot when the /start command is invoked
 def start(update, context):
     chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=chat_id, text="Paste a link and I'll unlock it...")
 
 def _transform_link(orig_link):
+    # first start with http cause sometimes the share button from android appends text at the beginning
+    orig_link = "http" + orig_link.split("http")[1]
+
+    # now handle the tokenization
     orig_link = orig_link.split("?")[0]
     orig_link = orig_link.split("/")
     result_link = []
@@ -40,16 +40,22 @@ def get_link(update, context):
     # format the data into a string
     # message = f"Word: {word}\n\nOrigin: {origin}\n{meanings}"
 
-# run the start function when the user invokes the /start command 
-dispatcher.add_handler(CommandHandler("start", start))
 
-# invoke the get_word_info function when the user sends a message 
-# that is not a command.
-dispatcher.add_handler(MessageHandler(Filters.text, get_link))
+telegram_bot_token = os.environ['TGRM_TKN']
+if telegram_bot_token != 'TEST':
+    updater = Updater(token=telegram_bot_token, use_context=True)
+    dispatcher = updater.dispatcher
 
-# updater.start_polling()
-updater.start_webhook(listen="0.0.0.0",
-                      port=int(os.environ.get('PORT', 5243)),
-                      url_path=telegram_bot_token,
-                      webhook_url= 'https://hrtzunlk.herokuapp.com/' + telegram_bot_token
-                      )
+    # run the start function when the user invokes the /start command 
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # invoke the get_word_info function when the user sends a message 
+    # that is not a command.
+    dispatcher.add_handler(MessageHandler(Filters.text, get_link))
+
+    # updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                        port=int(os.environ.get('PORT', 5243)),
+                        url_path=telegram_bot_token,
+                        webhook_url= 'https://hrtzunlk.herokuapp.com/' + telegram_bot_token
+                        )
